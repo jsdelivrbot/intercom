1a7bb1403f643682891c83a497c6c18b0ba61914
Yini Shi <yini.shi@gmail.com>
2017-11-18 10:31:44 -0600

    A bit of layout to prep for routes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import {MessengerService} from './core/messenger.service';
 import {ChatLogService} from './core/chat-log.service';
 import { ChatBoxComponent } from './chat-box/chat-box.component';
-
 
 @NgModule({
   declarations: [
