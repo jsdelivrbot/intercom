8ac4ca06610ce8ec1da48b8ba754544e260adef0
Yini Shi <yini.shi@gmail.com>
2017-11-19 16:30:09 -0600

    Cleanup

--- server/start.js
+++ server/start.js
@@ -3,9 +3,6 @@ const server = http.createServer();
 
 const express = require('express');
 const app = express();
-// const router = require('./routes');
-
-const path = require('path');
 
 server.on('request', app);
 
@@ -15,14 +12,6 @@ const PORT = process.env.PORT || 8888;
 require('./sockets.js')(server);
 
 /* START SERVER */
-server.listen(PORT, function() {
-    console.log('The server is listening on port ' + PORT);
+server.listen(PORT, function () {
+  console.log('The server is listening on port ' + PORT);
 });
-
-// app.use(express.static(path.join(__dirname, '../client')));
-
-/* ROUTES */
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'index.html'));
-// });
-
