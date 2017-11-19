76ddaf4a7db3fbabf004c97583825d22069d1e50
Yini Shi <yini.shi@gmail.com>
2017-11-19 09:51:07 -0600

    Created conversation class

--- client/src/app/core/models/conversation.model.ts
+++ client/src/app/core/models/conversation.model.ts
@@ -1,8 +1,20 @@
 import {IMessage} from './message.model';
 import {User} from './user.model';
 
-export interface Conversation {
-  visitor: User,
+export interface IConversation {
+  visitor: User;
   log: IMessage[];
   latestTime: number;
+}
+
+export class Conversation implements IConversation {
+  visitor: User;
+  log: IMessage[];
+  latestTime: number;
+
+  constructor(visitorId) {
+    this.visitor = new User(visitorId);
+    this.log = [];
+    this.latestTime = 0;
+  }
 }
